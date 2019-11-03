f954e7019fa8bd3af4ec6023db322459df1c8f13
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 23:37:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all(array)
+  new = []
+  (0..array.length - 1).step do |index|
+    yield(array[index])
+    new << yield(array[index])
+  end
+  new
 end
